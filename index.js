74f0f32a7cc0f52604c2954ab96858b34a7acf3c
muhmdajeer <muhdajeer@gmail.com>
2022-12-10 21:52:10 +0530

    created chat service

---
+++ index.js
@@ -0,0 +1,40 @@
+const dotenv = require('dotenv').config()
+const io = require('socket.io')(8800,{
+    cors : {
+        origin : process.env.CLIENT_URL
+    }
+})
+
+let activeUsers = [];
+
+io.on('connection',(socket)=>{
+    // console.log(socket);
+    socket.on('new-user-add',(newUserId)=>{
+        console.log(newUserId);
+        if(!activeUsers.some((user)=> user.userId === newUserId))
+        {
+            activeUsers.push({
+                userId : newUserId,
+                socketId : socket.id
+            })
+        }
+
+        io.emit('get-users',activeUsers);
+    })
+
+    socket.on('send-message',(data)=>{
+        console.log(data);
+        const {receiverId} = data;
+        const user = activeUsers.find((user)=> user.userId === receiverId)
+        if(user){
+            io.to(user.socketId).emit('receive-message',data)
+        }
+
+    })
+
+    socket.on('disconnect',()=>{
+        activeUsers = activeUsers.filter((user)=> user.socketId !== socket.id)
+        io.emit('get-users',activeUsers)
+    })
+
+})
